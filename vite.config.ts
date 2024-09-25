9d7413591d0ffb91f48700a0f186bd8bd6955856
Philip Saa <cowglow@gmail.com>
2024-09-25 19:43:49 +0200

    Update base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/react-hook-form",
+  base:"/rhf-complex-input-control-form",
   plugins: [react(), checker({typescript:true})],
 })

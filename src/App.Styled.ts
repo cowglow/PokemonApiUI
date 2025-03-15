9fa8952ae839cbf13b68d6be8a36664fdd518eb3
Philip Saa <cowglow@gmail.com>
2025-03-15 07:57:28 +0100

    Style clean up

--- src/App.Styled.ts
+++ src/App.Styled.ts
@@ -1,16 +1,7 @@
-import {Box, Paper, styled} from "@mui/material";
+import {Box, styled} from "@mui/material";
 
-export const Container = styled(Paper)`
-    border: solid red;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    //flex-direction: column;
-    //overflow: scroll;
-`;
 
 export const LayoutWrapper = styled(Box)`
-    border: solid greenyellow;
     display: flex;
     flex-direction: column;
     height: 100svh;

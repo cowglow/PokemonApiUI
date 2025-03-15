2adf38de03df99aa115ea505a79cae6d35fd7625
Philip Saa <cowglow@gmail.com>
2025-03-15 07:00:16 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/rhf-complex-input-control-form",
+  base:"/PokemonApiUI",
   plugins: [react(), checker({typescript:true})],
 })
